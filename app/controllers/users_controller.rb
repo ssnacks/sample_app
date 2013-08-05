395699b685ebf400c4dd068cb06a88d6371398a6
Christopher Morton <ssnacks@gmail.com>
2013-08-05 00:51:29 -0700

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,14 +7,27 @@ class UsersController < ApplicationController
   	@user = User.new
   end
 
+
   def create
     @user = User.new(params[:user])
     if @user.save
     	sign_in @user
       flash[:success] = "Welcome to the Sample App!"
-    	redirect_to @user
+      redirect_to @user
     else
       render 'new'
     end
   end
+  def edit
+    @user = User.find(params[:id])
+  end
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      # Handle a successful update.
+    else
+      render 'edit'
+    end
+  end
 end
+
